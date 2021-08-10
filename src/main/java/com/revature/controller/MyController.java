4082d7a0326fb4283599dd06fb30e2f56ef0d469
Rajesh Nallusamy <rajesh.kit@gmail.com>
2021-08-10 08:43:58 +0530

    first commit

---
+++ src/main/java/com/revature/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.revature.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@GetMapping("/greet")
+	public String hello() {
+		return "Hello Rajesh";
+	}
+}
